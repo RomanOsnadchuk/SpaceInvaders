ead5a63d681b0d0b07742a63d0c9dc0fcd4172be
Sviatoslav Filippov <filippov.sviatoslav@gmail.com>
2021-07-20 20:27:27 +0300

    start

---
+++ ConsoleApplication/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Dynamic;
+
+namespace ConsoleApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Game game = new Game();
+            game.Height = 20;
+            game.Width = 50;
+            //Console.BufferHeight = game.Height;
+            //Console.BufferWidth = game.Width;
+            
+            char[,] Pos = new char[game.Height,game.Width];
+            for (int i = 0; i < game.Height; i++)
+            {
+                for (int j = 0; j < game.Width; j++)
+                {
+                    Pos[i, j] = '.';
+                }
+            }
+
+            DrawField(game, Pos);
+            
+            
+
+            Console.ReadLine();
+        }
+
+        private static void DrawField(Game game, char[,] Pos)
+        {
+            for (int i = 0; i < game.Height; i++)
+            {
+                for (int j = 0; j < game.Width; j++)
+                {
+                    Console.Write(Pos[i, j]);
+                }
+
+                Console.WriteLine("");
+            }
+        }
+    }
+
+    class Game
+    {
+        public int Width { get; set; }
+        public int Height { get; set; }
+        
+        
+    }
+
+    class Starship
+    {
+        public char Body { get; set; }
+        private Coordynate position = new Coordynate();
+    }
+
+    class Invaider
+    {
+        public char Body { get; set; }
+        private Coordynate position = new Coordynate();
+    }
+
+    class Coordynate
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+
+        public void Move(int deltaX, int deltaY)
+        {
+            
+        }
+    }
+    
+}
