86ef4d889beede033c2d077c080ff855ce55e9b4
RomanOsnadchuk <roman.osnadchuk@gmail.com>
2021-09-06 18:04:30 +0300

    Refactoring

--- WPFapplication/MainWindow.xaml.cs
+++ WPFapplication/MainWindow.xaml.cs
@@ -19,9 +19,9 @@ namespace WPFapplication
         DispatcherTimer timer = new DispatcherTimer();
 
         int speed = 10;
-        int dropSpeed = 10;
-        bool goLeft, goRight, Shot, InvaderLive = true;
+        bool start = true, goLeft, goRight, Shot, InvaderLive = true;
         List<Rectangle> Bullets = new List<Rectangle>();
+        List<Rectangle> Invaiders = new List<Rectangle>();
 
         public MainWindow()
         {
@@ -29,35 +29,23 @@ namespace WPFapplication
             myCanvas.Focus();
             timer.Tick += MainTimerEvent;
             timer.Interval = TimeSpan.FromMilliseconds(20);
-            timer.Start();
+
+            if (start) timer.Start();
         }
 
         private void MainTimerEvent(object sender, EventArgs e)
         {
-           
 
-            if (goLeft == true && Canvas.GetLeft(starship) > 0)
-            {
-                Canvas.SetLeft(starship, Canvas.GetLeft(starship) - speed);
-            }
-            if (goRight == true && Canvas.GetLeft(starship) + (starship.Width + 24) < Application.Current.MainWindow.Width)
-            {
-                Canvas.SetLeft(starship, Canvas.GetLeft(starship) + speed);
-            }
-
-            if (Shot == true)
-            {
-                Fire();
-            }
             if (InvaderLive)
             {
+                MoveOnPress(starship, speed);
                 if (Canvas.GetLeft(invaider) + invaider.Width + 50 < Application.Current.MainWindow.Width)
                 {
-                    Canvas.SetLeft(invaider, Canvas.GetLeft(invaider) + speed);
+                    InvaidersAtacks();
                 }
                 else
                 {
-                    Canvas.SetLeft(invaider, 10);
+                    Canvas.SetLeft(invaider, 2);
                     Canvas.SetTop(invaider, Canvas.GetTop(invaider) + invaider.Height);
                 }
             }
@@ -66,13 +54,13 @@ namespace WPFapplication
                 invaider.Fill = Brushes.Gray;
             }
 
-            if (Bullets != null)
+            if (Bullets != null && Bullets.Count != 0)
             {
                 for (int i = 0; i < Bullets.Count; i++)
                 {
-                    Canvas.SetTop(Bullets[i], Canvas.GetTop(Bullets[i]) - 30);
-                    if (Canvas.GetTop(Bullets[i]) < 20) myCanvas.Children.Remove(Bullets[i]);
-                    if ((Canvas.GetLeft(Bullets[i]) > Canvas.GetLeft(invaider)) && (Canvas.GetLeft(Bullets[i]) < Canvas.GetLeft(invaider) + invaider.Width) && (Canvas.GetTop(Bullets[i]) > Canvas.GetTop(invaider)) && (Canvas.GetTop(Bullets[i]) < Canvas.GetTop(invaider) + invaider.Height)) DieInvaider();
+                    Canvas.SetTop(Bullets[i], Canvas.GetTop(Bullets[i]) - 10);
+                    if (Colision(Bullets[i], invaider)) { DieInvaider(); myCanvas.Children.Remove(Bullets[i]);  Bullets.RemoveAt(i); break; }
+                    if (Canvas.GetTop(Bullets[i]) < 20) { myCanvas.Children.Remove(Bullets[i]);  Bullets.RemoveAt(i); }
                 }
             }
 
@@ -90,7 +78,12 @@ namespace WPFapplication
             }
             if (e.Key == Key.Space)
             {
-                Fire();
+                if (Shot) Fire();
+                Shot = false;
+            }
+            if (e.Key == Key.Enter)
+            {
+                start = true;
             }
         }
 
@@ -106,28 +99,47 @@ namespace WPFapplication
             }
             if (e.Key == Key.Space)
             {
-                Shot = false;
+                Shot = true;
             }
         }
 
         private void DieInvaider()
         {
-            // invaider.Visibility = Visibility.Hidden;
-            //myCanvas.Children.Remove(invaider);
             InvaderLive = false;
             win.Visibility = Visibility.Visible;
         }
 
         private void Fire()
         {
-            //Rectangle NewRect = new Rectangle { Height = bullet.Height, Width = bullet.Width, Fill = Brushes.Blue, Visibility = Visibility.Visible };
-            //Canvas.SetLeft(NewRect, Canvas.GetLeft(starship));
-            //Canvas.SetTop(NewRect, Canvas.GetTop(starship));
             Bullets.Add(new Rectangle { Height = bullet.Height, Width = bullet.Width, Fill = Brushes.Blue });
             Canvas.SetLeft(Bullets[Bullets.Count - 1], Canvas.GetLeft(starship));
             Canvas.SetTop(Bullets[Bullets.Count - 1], Canvas.GetTop(starship));
             myCanvas.Children.Add(Bullets[Bullets.Count-1]);
         }
        
+        private void MoveOnPress(Rectangle ship, int speed)
+        {
+            if (goLeft == true && Canvas.GetLeft(ship) > 0)
+            {
+                Canvas.SetLeft(starship, Canvas.GetLeft(ship) - speed);
+            }
+            if (goRight == true && Canvas.GetLeft(ship) + (ship.Width + 24) < Application.Current.MainWindow.Width)
+            {
+                Canvas.SetLeft(starship, Canvas.GetLeft(ship) + speed);
+            }
+        }
+
+        private void InvaidersAtacks()
+        {
+            Canvas.SetLeft(invaider, Canvas.GetLeft(invaider) + speed);
+        }
+
+        private bool Colision(Rectangle Body1, Rectangle Body2)
+        {
+            return (Canvas.GetLeft(Body1) > Canvas.GetLeft(Body2))
+                && (Canvas.GetLeft(Body1) < Canvas.GetLeft(Body2) + Body2.Width)
+                && (Canvas.GetTop(Body1) > Canvas.GetTop(Body2))
+                && (Canvas.GetTop(Body1) < Canvas.GetTop(Body2) + Body2.Height);
+        }
     }
 }
