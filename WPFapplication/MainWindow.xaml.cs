122122e53d6d911b23f089fee8201674a448e6c0
RomanOsnadchuk <roman.osnadchuk@gmail.com>
2021-12-08 13:38:19 +0200

    attempt

--- WPFapplication/MainWindow.xaml.cs
+++ WPFapplication/MainWindow.xaml.cs
@@ -1,7 +1,10 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Shapes;
 using System.Windows.Threading;
 using Core;
 
@@ -10,21 +13,30 @@ namespace WPFapplication
     public partial class MainWindow : Window
     {
         private readonly Game game;
+        private List<Ellipse> winAlient;
         private readonly DispatcherTimer timer = new DispatcherTimer();
-        private bool InvaderLive = true;
-        private bool invaiderDir = true;
+        //private bool InvaderLive = true;
+        //private bool invaiderDir = true;
         private bool readyShot;
 
-        private int speed = 10;
+        //private int speed = 10;
         private bool StartGame;
 
         public MainWindow()
         {
             InitializeComponent();
             myCanvas.Focus();
-            game = new Game(20, 20, 1, ' ');
+
+            game = new Game(20, 20, 2, ' ');
+            winAlient = new List<Ellipse>();
+
+            for (int i = 0; i < 2; i++)
+            {
+                winAlient.Add(new System.Windows.Shapes.Ellipse() { Height = 29, Width = 14, Fill = Brushes.Red, Visibility = Visibility.Visible, IsEnabled = true});
+            }
+
             timer.Tick += MainTimerEvent;
-            timer.Interval = TimeSpan.FromMilliseconds(1000 / 50);
+            timer.Interval = TimeSpan.FromMilliseconds(1000 / 10);
             timer.Start();
         }
 
@@ -33,15 +45,29 @@ namespace WPFapplication
         {
             if (StartGame)
             {
-                //GameBox.Clear();
-
                 if (Start.Visibility != Visibility.Hidden) Start.Visibility = Visibility.Hidden;
-                // game.UpdateField();
-                //DrawField(game.Field, GameBox);
-                Canvas.SetLeft(SS, 25 + (gameWindow.ActualWidth - 25*2) / game.Field.Width * game.Starship.Position.X);
+                UpdatePos(SS, game.Starship);
+
+
+                for (int i = 0; i < game.Aliens.Count; i++)
+                {
+                    UpdatePos(winAlient[i], game.Aliens[i]);
+                }
                 game.MoveAliens(1, 0);
                 game.MoveShot(0, -1);
                 game.Collision();
+            }
+
+            void UpdatePos(System.Windows.Shapes.Shape shape, GameObject gObject)
+            {
+                double gameBorder = 50;
+                Canvas.SetLeft(shape, calcMidl(gameWindow.ActualWidth, game.Field.Width, gObject.Position.X, shape.Width));
+                Canvas.SetTop(shape, calcMidl(gameWindow.ActualHeight, game.Field.Height, gObject.Position.Y, shape.Height));
+
+                double calcMidl(double win, double field, double pos, double shape)
+                {
+                    return gameBorder + (win - 2 * gameBorder) / field * pos + (win - 2 * gameBorder) / (2 * field) - shape / 2;
+                }
             }
         }
 
